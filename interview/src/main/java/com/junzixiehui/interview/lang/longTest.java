dbc5b658d56cb40ca616afc6d9848ef9cd15d572
曲利斌 <qulibin@edaijia-inc.cn>
2018-07-13 14:36:12 +0800

    add

---
+++ interview/src/main/java/com/junzixiehui/interview/lang/longTest.java
@@ -0,0 +1,15 @@
+package com.junzixiehui.interview.lang;
+
+/**
+ * <p>Description: </p>
+ * @author: by qulibin
+ * @date: 2018/7/13  上午11:33
+ * @version: 1.0
+ */
+public class longTest {
+
+
+	public static void main(String[] args) {
+		System.out.println(3*0.1 == 0.3);
+	}
+}
